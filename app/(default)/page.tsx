cbfaaf8d7ff0f9f8198108e73b84676f981c02c9
Alexandru R. Beldiman <roberto.beldiman10@gmail.com>
2023-06-05 12:12:22 +0300

    Last changes

--- app/(default)/page.tsx
+++ app/(default)/page.tsx
@@ -1,6 +1,6 @@
 export const metadata = {
-  title: "Home - Simple",
-  description: "Page description",
+  title: "Lucbtrans",
+  description: "Moving Company",
 };
 
 import Hero from "@/components/hero";
